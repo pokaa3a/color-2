fa78c914b371d28ea1414d63e6cbe4d4cadba32e
pokaa3a <pkhuang@umich.edu>
2021-09-08 22:52:09 -0700

    first commit

---
+++ Soordinator/Starter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// The first script when the scene is loaded
+class Starter
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void OnBeforeSceneLoadRuntimeMethod()
+    {
+        Debug.Log("Before first Scene loaded");
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    static void OnAfterSceneLoadRuntimeMethod()
+    {
+        // Debug.Log("After first Scene loaded");
+    }
+
+    [RuntimeInitializeOnLoadMethod]
+    static void OnRuntimeMethodLoad()
+    {
+        // Debug.Log("RuntimeMethodLoad: After first Scene loaded");
+    }
+}
